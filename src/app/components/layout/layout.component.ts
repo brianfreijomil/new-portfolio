e34730e0cee846611f8e56c2eea0d09b20f6b544
Brian Freijomil <brianfreijomil@hotmail.com>
2024-05-27 19:06:43 -0300

    home done, projects section done

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../reusable/header/header.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,RouterLink,RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
--- src/app/components/layout/projects/projects.component.ts
+++ src/app/components/layout/projects/projects.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../reusable/header/header.component';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,RouterLink,CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+
+  hoverCard: boolean = false;
+  projects: any[] = [
+    {
+      id: 1,
+      title: 'ScoutingPro',
+      description: 'Application designed and developed to facilitate the scouting work of soccer teams. Personal project. (2024)',
+      links: [
+        'https://github.com/brianfreijomil/ScoutingAppFrontend',
+        'https://github.com/brianfreijomil/ScoutingApiBack'
+      ]
+    },
+    {
+      id: 2,
+      title: 'ScooterApp',
+      description: 'Basic application designed to manage the use of scooters and their trips. Project done at the university. (2023)',
+      links: [
+        'https://github.com/brianfreijomil/AppMonopatines',
+      ]
+    },
+    {
+      id: 3,
+      title: 'RetroGames',
+      description: 'Application designed to play online games, especially retro games. Project done for the university. (2022)',
+      links: [
+        'https://github.com/brianfreijomil/Interfaces.github.io',
+        'https://brianfreijomil.github.io/Interfaces.github.io/Entrega-Final/community.html'
+      ]
+    }
+  ];
+
+  constructor() {}
+
+  onHover() {
+    this.hoverCard = true;
+    console.log(true);
+  }
 
 }
